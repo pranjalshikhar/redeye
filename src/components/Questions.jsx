c96b9563dbe607eb5fb2c399680740e109179117
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-01-23 15:53:44 +0530

    Update Questions.jsx

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,40 +1,44 @@
 const Questions = ({ topics, handleAnswerOptionClick, currentTopic }) => {
   return (
-    <div className="wrapper column">
-      <div className="question-section">
-        <div className="question-title">
-          <h2>{topics[currentTopic].categoryTitle}</h2>
-          <div className="question-count">
-            Step {currentTopic + 1} of {topics.length}
+    <>
+      {topics.map((topic, index) => (
+        <div className="wrapper column" key={index}>
+          <div className="question-section">
+            <div className="question-title">
+              <h2>{topics[currentTopic].categoryTitle}</h2>
+              <div className="question-count">
+                Step {currentTopic + 1} of {topics.length}
+              </div>
+            </div>
+            <div className="question-description">
+              <p>{topics[currentTopic].categoryDescription}</p>
+            </div>
+          </div>
+          <div className="answer-section d-flex">
+            {topics[currentTopic].answerOptions.map((answerOption, i) => (
+              <div className="answer-item" key={i}>
+                <button
+                  onClick={() =>
+                    handleAnswerOptionClick(
+                      answerOption.ageRating,
+                      topics[currentTopic].categoryTitle
+                    )
+                  }
+                  key={i++}
+                >
+                  <span className="d-flex align-items-center">
+                    <svg height="20" width="20">
+                      <circle cx="10" cy="10" r="8" fill="red" />
+                    </svg>
+                    {answerOption.answerText}
+                  </span>
+                </button>
+              </div>
+            ))}
           </div>
         </div>
-        <div className="question-description">
-          <p>{topics[currentTopic].categoryDescription}</p>
-        </div>
-      </div>
-      <div className="answer-section d-flex">
-        {topics[currentTopic].answerOptions.map((answerOption, i) => (
-          <div className="answer-item" key={i}>
-            <button
-              onClick={() =>
-                handleAnswerOptionClick(
-                  answerOption.ageRating,
-                  topics[currentTopic].categoryTitle
-                )
-              }
-              key={i++}
-            >
-              <span className="d-flex align-items-center">
-                <svg height="20" width="20">
-                  <circle cx="10" cy="10" r="8" fill="red" />
-                </svg>
-                {answerOption.answerText}
-              </span>
-            </button>
-          </div>
-        ))}
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
 
