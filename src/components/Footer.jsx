71d3f4199729202d109d0d37fd7c77390f1fef93
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-01-24 23:29:13 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "./images/logo.svg";
+import { ReactComponent as Logo } from "../images/logo.svg";
 
 const Footer = () => {
   return (
